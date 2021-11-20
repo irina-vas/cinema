8f88c6c03760f01673c3412046e99ad4c500ab91
irina-vas <67779496+irina-vas@users.noreply.github.com>
2021-11-20 22:43:15 +0200

    Update Schedule.js

--- cinema-app/src/components/Schedule.js
+++ cinema-app/src/components/Schedule.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchSchedule } from '../Redux/asynAction';
 import Loader from './Loader';
 import Modal from './Modal';
+import img_headline from '../images/Без имени-1.png'
 
 function Schedule() {
   const dispatch = useDispatch();
@@ -12,12 +13,11 @@ function Schedule() {
   const input = useRef();
   const [scheduleMovie, setScheduleMovie] = useState('');
   const [modalOpen, setModalOpen] = useState(false);
+  const [modalSecond, setModalSecond] = useState(false);
 
   useEffect(() => {
     dispatch(fetchSchedule());
   }, [dispatch]);
-
-  console.log(schedule);
 
 
   if (loading) {
@@ -38,46 +38,52 @@ function Schedule() {
     return date.slice(11,16)
   }
   
-  let filteredMovie = schedule.filter(item => {
-    if (scheduleMovie === getDate(item.date)) {
-      return item
-    }
-  })
+  let filteredMovie = schedule.filter(item => scheduleMovie === getDate(item.date) ? item : null)
 
   function modalClose() {
     return setModalOpen(false)
+
   }
 
-
-  console.log(filteredMovie)
-
+  function modalSecClose() {
+    return setModalSecond(false)
+  }
 
   return (
     <div className="schedule_wrapper">
-       <Modal modalOpen={modalOpen} modalClose={modalClose} />
-      <form onChange={(e) => handleInput('2021-10-01')}>
-        <input type="date" ref={input} />
-      </form>
-     
+      <Modal 
+        modalSecClose={modalSecClose} 
+        modalOpen={modalOpen} 
+        modalClose={modalClose} 
+        modalSecond={modalSecond}  
+        setModalSecond={ setModalSecond} 
+      />
+      <div className="wrapper_image_input">
+        <div className="schedule_image">
+          <img src={img_headline} alt="headline" />
+        </div>
+        <form onChange={(e) => handleInput('2021-10-01')}>
+          <input type="date" ref={input} id="chose_date" />
+        </form>
+      </div>
 
- 
-      {filteredMovie.map((item, index)=> {
-        
-        return (
-
-          <div key={index} className="schedule_container">
-            <div className="schedule_poster"><img src={item.movie.poster} alt={item.title} /></div>
-            <div className="schedule_desc">
-              <div className="schedule_title">{item.movie.title}</div>
-              <div className="schedule_date">{getDate(item.date)}</div>
-              <div className="schedule_time">{getTime(item.date)}</div>
+      <div className="schedule_movie_wrapper">
+        {filteredMovie.map((item, index)=> {
+          
+          return (
+            <div key={index} className="schedule_container">
+            <div className="schedule_poster">
+              <img src={item.movie.poster} alt={item.title} />
             </div>
-            <button className="buyTicket" onClick={() => setModalOpen(true)}>КУПИТЬ</button>
-          </div>
-        )
-      })
-      } 
-
+              <div className="schedule_desc">
+                <div className="schedule_title">{item.movie.title}</div>
+                <div className="schedule_time">{getTime(item.date)}</div>
+              </div>
+              <button className="buyBtn" onClick={() => setModalOpen(true)}>КУПИТЬ</button>
+            </div>
+          )
+        })} 
+      </div>
     </div>
   );
 }
