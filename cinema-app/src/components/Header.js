4e02bbd0f8775220c26dae52bab41a9dc42f50d3
irina-vas <67779496+irina-vas@users.noreply.github.com>
2021-11-20 22:39:55 +0200

    Update Header.js

--- cinema-app/src/components/Header.js
+++ cinema-app/src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import '../styles/styles.css';
-import user_icon from '../images/user_icon.png'
+import user_icon from '../images/user_icon.png';
+import cinema_icon from '../images/1cinema_icon.png'
+import cinema_icon_reel from '../images/88502-middle.png'
 
 function Header() {
   return (
     <div className="wrapper_header">
-      <h2>CINEMA</h2>
-      <div className="user_info">Vasya</div>
+      <div className="cinema_icon">
+        <img src={cinema_icon} alt="cinema_icon"/>
+      </div>
+      <div className="cinema_icon_reel">
+        <img src={cinema_icon_reel} alt="cinema_icon"/>
+      </div>
+      <div className="user_info">
+        <p>Hi, Irina</p>
+        <div className="user_icon">
+          <img src={user_icon} alt="user"/>
+        </div>
+      </div>
+      
     </div>
   );
 }
