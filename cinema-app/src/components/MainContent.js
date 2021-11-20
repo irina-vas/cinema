948d83aee6dabb25b727021aabfb7752dda24431
irina-vas <67779496+irina-vas@users.noreply.github.com>
2021-11-20 22:40:54 +0200

    Update MainContent.js

--- cinema-app/src/components/MainContent.js
+++ cinema-app/src/components/MainContent.js
@@ -9,13 +9,9 @@ function MainContent() {
 
   const dispatch = useDispatch();
   const movies = useSelector(state => state.movies);
-  const schedule = useSelector(state => state.schedule);
   const loading = useSelector(state => state.loading);
-  let scheduleId = [];
-  let moviesOnScreen = [];
-  let genres = ['биография', 'боевик', 'детектив', 'драма', 'комедия', 'криминал', 'приключения', 'семейный', 'триллер', 'фэнтези', 'фантастика']
-  const [movieByGenre, setMovieByGenre] = useState([]);
-  const [movieByName, setMovieByName] = useState([])
+  const [movieByGenre, setMovieByGenre] = useState('жанр');
+  const [movieByName, setMovieByName] = useState('')
 
 
   useEffect(() => {
@@ -26,8 +22,6 @@ function MainContent() {
     dispatch(fetchSchedule());
   }, [dispatch]);
 
-  console.log(movies);
-
 
   if (loading) {
     return (
@@ -35,65 +29,49 @@ function MainContent() {
     )
   }
 
-  for (let item of schedule) {
-    if (scheduleId.includes(item.movie.id)) {
-        continue;
-    } else {
-      scheduleId.push(item.movie.id)
-    }
-  }
-
-  for (let item of movies) {
-    if (scheduleId .includes(item.id)) {
-      moviesOnScreen.push(item)
-    }
-  }
-
   const handleMovieByGenre = (e) => {
     setMovieByGenre(e.target.value)
   }
-
-  let filteredMovieByGenre = movies.filter(item => {
-    if (item.genre.includes(movieByGenre)) {
-      return item
-    }
-  })
-
-  console.log(filteredMovieByGenre)
 
   const handleMovieByName = (e) => {
     setMovieByName(e.target.value)
   }
 
-  let filteredMovieByName = movies.filter(item => {
-    if (item.title.includes(movieByName)) {
-      return item;
-    }
-  })
+  let moviesFiltered=[];
+   if (movieByGenre === 'жанр' && !movieByName) {
+    moviesFiltered = movies.filter(item => item)
+  }  else if (movieByName !== '') {
+    moviesFiltered = movies.filter(item => item.title.toLowerCase().includes(movieByName.toLowerCase()))
+  } else if (movieByGenre !== 'жанры') {
+    moviesFiltered = movies.filter(item => item.genre.includes(movieByGenre))
+  } else if (movieByName && movieByGenre !== 'жанры'){
+    moviesFiltered = movies.filter(item => item.title.toLowerCase().includes(movieByName.toLowerCase()) && item.genre.includes(movieByGenre))
+  }
 
-  console.log(filteredMovieByName)
   return (
     <div className="container_main">
-      <input type="text" className="search_movies_input" onChange={handleMovieByName} />
-      <select onChange={handleMovieByGenre} >
-        <option>жанр</option>
-        <option value='биография'>биография</option>
-        <option value='боевик'>боевик</option>
-        <option value='детектив'>детектив</option>
-        <option value='драма'>драма</option>
-        <option value='комедия'>комедия</option>
-        <option value='криминал'>криминал</option>
-        <option value='приключения'>приключения</option>
-        <option value='семейный'>семейный</option>
-        <option value='триллер'>триллер</option>
-        <option value='фэнтези'>фэнтези</option>
-        <option value='фантастика'>фантастика</option>
-      </select>
+      <div className="main_input" id="main_input">
+        <input type="text" className="search_movies_input" onChange={handleMovieByName} />
+        <select onChange={handleMovieByGenre} className="select_genres">
+          <option>жанр</option>
+          <option value='биография'>биография</option>
+          <option value='боевик'>боевик</option>
+          <option value='детектив'>детектив</option>
+          <option value='драма'>драма</option>
+          <option value='комедия'>комедия</option>
+          <option value='криминал'>криминал</option>
+          <option value='приключения'>приключения</option>
+          <option value='семейный'>семейный</option>
+          <option value='триллер'>триллер</option>
+          <option value='фэнтези'>фэнтези</option>
+          <option value='фантастика'>фантастика</option>
+        </select>
+      </div>
       <div className="wrapper_movies">
 
-      {moviesOnScreen.map((item, index) => {
+      {moviesFiltered.map((item, index) => {
         return (
-          <div className="moviesOnScreen_cards">
+          <div className="moviesOnScreen_cards" key={index}>
             <Link to={`/movies/${item.title}`}>
               <div className="movie_poster">
                 <img src={item.poster} alt={item.title} />
