4a65a2351ce27be89190e7c79981dfc319faf87a
irina-vas <67779496+irina-vas@users.noreply.github.com>
2021-11-20 22:42:39 +0200

    Update Nav.js

--- cinema-app/src/components/Nav.js
+++ cinema-app/src/components/Nav.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../styles/styles.css';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Nav() {
+  const location = useLocation();
+
   return (
     <div className="wrapper_nav">
       <ul className="nav">
       <li className="nav_item">
-        <Link to="/movies">HOME</Link>
+        <Link to="/" className={ location.pathname === '/' ? 'active' : '' }>HOME</Link>
       </li>
       <li className="nav_item">
-        <Link to="/schedule">SCHEDULE</Link>
+        <Link to="/movies" className={ location.pathname === '/movies' ? 'active' : ''}>ON SCREEN</Link>
+      </li>
+      <li className="nav_item">
+        <Link to="/schedule" className={ location.pathname === '/schedule' ? 'active' : ''}>SCHEDULE</Link>
       </li>
       </ul>
     </div>
